bd9839e6f02115cafdd1ba572295ffdd4ab31502
Varun3972 <96814526+Varun3972@users.noreply.github.com>
2021-12-29 22:25:30 +0530

    Create program(5).c

---
+++ program(5).c
@@ -0,0 +1,16 @@
+lude<stdio.h>
+#include<conio.h>
+void main()
+{
+      int i,j,n;
+      printf("enter the no.of rows");
+      scanf("%d",&n);
+      for(i=1;i<=n,i++);
+      {
+          for(j=1;j<=n;j++);
+          {
+               printf("%d\t,j");
+          }
+           printf("\n");
+      }
+}      
