71533091be12386a509d09553ebfe4cf924a6611
Varun3972 <96814526+Varun3972@users.noreply.github.com>
2021-12-29 22:18:56 +0530

    Update program(2).c

--- program(2).c
+++ program(2).c
@@ -7,9 +7,9 @@ void main()
       scanf("%d",&n);
       for(i=1;i<=n,i++);
       {
-          for(j=1;j,=n;j++);
+          for(j=1;j<=n;j++);
           {
-               PRINTF("\n");
+               PRINTF("*\t");
           }
            printf("\n");
       }
